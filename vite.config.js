8b7a308dcc3ad27c505ba6dd778ae13c7a192b02
Serobles <adserobles@gmail.com>
2026-05-21 12:47:57 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Permite acceder a import.meta.env.VITE_MODE en el código
+    'import_meta_env': JSON.stringify({
+      VITE_MODE: process.env.VITE_MODE || 'admin'
+    })
+  }
 })
